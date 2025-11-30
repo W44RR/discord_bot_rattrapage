5f90e3cdc0614b399275cf44242c4b6b6f226d9f
omaylesm <oukacha.mayles@ynov.com>
2025-11-30 19:18:32 +0100

    petite erreur sur les commandes, doit modifier l'arbre après

--- commandes/discussion.py
+++ commandes/discussion.py
@@ -31,8 +31,8 @@ class Discussion(commands.Cog):
         
         self.bot.discussion_tree.set_root(root)
 
-@commands.Cog.listener()
-async def on_message(self, message):
+    @commands.Cog.listener()
+    async def on_message(self, message):
         if message.author == self.bot.user:
             return
 
@@ -58,18 +58,18 @@ async def on_message(self, message):
                     await message.channel.send("Erreur dans l'arbre, fin de la discussion.")
                     del self.user_discussions[message.author.id]
 
-@commands.command(name='help')
-async def start_discussion(self, ctx):
+    @commands.command(name='help')
+    async def start_discussion(self, ctx):
         self.user_discussions[ctx.author.id] = self.bot.discussion_tree.root
         await ctx.send(f"Début du questionnaire : {self.bot.discussion_tree.root.text} (Répondez par 'oui' ou 'non')")
 
-@commands.command(name='reset')
-async def reset_discussion(self, ctx):
+    @commands.command(name='reset')
+    async def reset_discussion(self, ctx):
         self.user_discussions[ctx.author.id] = self.bot.discussion_tree.root
         await ctx.send(f"Discussion réinitialisée : {self.bot.discussion_tree.root.text}")
 
-@commands.command(name='speak')
-async def speak_about(self, ctx, *args):
+    @commands.command(name='speak')
+    async def speak_about(self, ctx, *args):
         if len(args) >= 2 and args[0] == "about":
             topic = " ".join(args[1:])
             found = self.bot.discussion_tree.search_topic(self.bot.discussion_tree.root, topic)
