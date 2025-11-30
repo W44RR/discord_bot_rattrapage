9e2c60649d585eef8874848ea32b0fe22ed3a476
omaylesm <oukacha.mayles@ynov.com>
2025-11-30 16:50:32 +0100

    Les 3 trucs extra a faire, infos du serv, reverse de mot ou texte peut importe, et un lancé de dés.

--- commandes/extras.py
+++ commandes/extras.py
@@ -6,7 +6,6 @@ class Extras(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Fonctionnalité Bonus 1 : Jeu de dés
     @commands.command(name='roll')
     async def roll_dice(self, ctx, sides: int = 6):
         """Lance un dé à N faces (par défaut 6)"""
@@ -16,14 +15,12 @@ class Extras(commands.Cog):
         result = random.randint(1, sides)
         await ctx.send(f"🎲 Vous avez lancé un dé à {sides} faces et obtenu : **{result}**")
 
-    # Fonctionnalité Bonus 2 : Inversion de texte
     @commands.command(name='reverse')
     async def reverse_text(self, ctx, *, text: str):
         """Renvoie le texte inversé"""
         reversed_text = text[::-1]
         await ctx.send(f"🔄 : {reversed_text}")
 
-    # Fonctionnalité Bonus 3 : Infos du serveur
     @commands.command(name='serverinfo')
     async def server_info(self, ctx):
         """Affiche les informations du serveur"""
@@ -32,7 +29,6 @@ class Extras(commands.Cog):
         embed.add_field(name="Membres", value=guild.member_count, inline=True)
         embed.add_field(name="Salons", value=len(guild.channels), inline=True)
         
-        # Gestion sécurisée du propriétaire (peut être None sans les intents)
         if guild.owner:
             embed.add_field(name="Propriétaire", value=guild.owner.name, inline=True)
         else:
